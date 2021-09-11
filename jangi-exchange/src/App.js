f4f03d1214ee23ae7ba58cb826800e74cdd2b801
gabrielzencha <gzencha.ashungafac@tedu.edu.tr>
2021-09-11 19:52:25 +0300

    Header component started app structure defined

---
+++ jangi-exchange/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import MyHeader from './Components/MyHeader';
+function App() {
+  return (
+    <MyHeader/>
+    
+  );
+}
+
+export default App;
