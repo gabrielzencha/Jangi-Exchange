1da6c8077f2951cc63db4710b0267df204b325dd
siegkamgo <kamgosiegfried04@gmail.com>
2021-09-12 12:30:07 +0300

    I created the Header and did some modifications

--- jangi-exchange/src/Components/MyHeader.js
+++ jangi-exchange/src/Components/MyHeader.js
@@ -1,9 +1,39 @@
 import React from 'react'
-
+import './myheader.css';
 export default function MyHeader() {
     return (
-        <div>
-           <p>header component</p>
+        <div className = "headerBody">
+           <div className = "maindiv1">
+               JANGI Exchange
+           </div>
+           <div className = "maindiv2">
+                <div className = "div1">
+                    Buy Crypto(FCFA)
+                </div>
+                <div className = "div2">
+                    Markets
+                </div>
+                <div className = "div3">
+                    Swap
+                </div>
+                <div className = "div4">
+                        Finance
+                </div>
+           </div>
+           <div className = "maindiv3">
+                <div className = "div5">
+                    Register
+                </div>
+                <div className = "div6">
+                    Login
+                </div>
+                <div className = "div7">
+                    Language
+                </div>
+                <div className = "div8">
+                    Currency
+                </div>
+           </div>
         </div>
     );
 }
